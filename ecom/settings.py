9049e89ce04fe6713e40201d8e2d2dff882b5e3c
Waseemint <161630300+Waseemint@users.noreply.github.com>
2024-06-13 17:35:10 +0500

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -176,16 +176,16 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
 # DEFAULT_EMAIL
-# DEFAULT_FROM_EMAIL = 'waseemint.pk@gmail.com'
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_EMAIL')
+DEFAULT_FROM_EMAIL = 'waseemint.pk@gmail.com'
+# DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_EMAIL')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('DEFAULT_EMAIL')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_USER = 'waseemint.pk@gmail.com'
-# EMAIL_HOST_PASSWORD = 'pgkzlwobwbbfzhky'
+# EMAIL_HOST_USER = os.environ.get('DEFAULT_EMAIL')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'waseemint.pk@gmail.com'
+EMAIL_HOST_PASSWORD = 'pgkzlwobwbbfzhky'
 
 
 
