679dc606458af47b1e217e8703c74eb1b326fdfa
Waseemint <161630300+Waseemint@users.noreply.github.com>
2024-06-13 15:28:31 +0500

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -17,8 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    "corsheaders",
+    'django.contrib.staticfiles', 
+    #   "corsheaders",
 
     'category',
     'accounts',
